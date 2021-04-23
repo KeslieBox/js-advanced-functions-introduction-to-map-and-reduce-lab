9a6359a14bcb9ead445b1211bd21e7e214a29182
KeslieBox <kesliebox@gmail.com>
2021-04-23 14:01:54 -0500

    commit tree 0120fefbc3c167af4a4d9093f7a0f07bbe1655ef

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for(let i = 0; i < sourceArray.length; i++){
+        newArray.push(sourceArray[i] * -1) 
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray){
+    let a = []
+    for(let i = 0; i < sourceArray.length; i++) {
+        a.push(sourceArray[i])
+    }
+    return a
+}
+
+function mapToDouble(sourceArray) {
+    let a = []
+    for(let i = 0; i < sourceArray.length; i++){
+        a.push(sourceArray[i] * 2) 
+    }
+    return a
+}
+
+function mapToSquare(sourceArray) {
+    let a = []
+    for(let i = 0; i < sourceArray.length; i++){
+        a.push(sourceArray[i] ** 2) 
+    }
+    return a
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint
+    for(let i = 0; i < sourceArray.length; i++){
+        total += sourceArray[i]
+    }
+    return  total
+}
+
+function reduceToAllTrue(sourceArray){
+    for(let i = 0; i < sourceArray.length; i++) {
+        console.log(!sourceArray[i])
+        if (!sourceArray[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for(let i = 0; i < sourceArray.length; i++){
+        if (sourceArray[i]) return true
+    }
+    return false
+}
